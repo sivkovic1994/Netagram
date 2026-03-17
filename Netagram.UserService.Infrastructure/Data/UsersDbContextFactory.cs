8806d647f620dbe3269ce7311cd01be5e23371a3
lordkratos944 <54755663+lordkratos944@users.noreply.github.com>
2026-03-17 15:59:21 +0100

    Add UsersDbContextFactory

---
+++ Netagram.UserService.Infrastructure/Data/UsersDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Netagram.UserService.Infrastructure.Data
+{
+    public class UsersDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
+    {
+        public UserDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.FullName, "Netagram.UserService.API"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+
+            return new UserDbContext(optionsBuilder.Options);
+        }
+    }
+}
