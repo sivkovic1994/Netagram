e1753257e6af95a2906ebe9fe1cb170c36bcf9ab
lordkratos944 <54755663+lordkratos944@users.noreply.github.com>
2026-03-23 19:34:58 +0100

    Modify Program.cs for FeedService

--- Netagram.FeedService.API/Program.cs
+++ Netagram.FeedService.API/Program.cs
@@ -1,23 +1,51 @@
-using Netagram.FeedService.API.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Netagram.FeedService.Application.Interfaces;
+using Netagram.FeedService.Infrastructure.Clients;
+using Netagram.FeedService.Infrastructure.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IFeedService, FeedService>();
+
+builder.Services.AddHttpClient<IUserServiceClient, UserServiceClient>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7017/"); // UserService URL
+});
+
+builder.Services.AddHttpClient<IPostServiceClient, PostServiceClient>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7040/"); // PostService URL
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!))
+    };
+});
 
 var app = builder.Build();
 
-// Global error handler middleware catches all unhandled exceptions, logs them, and returns a consistent JSON error response to the client.
-// Add this middleware at the start of the pipeline to ensure all errors are handled in one place.
-app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
